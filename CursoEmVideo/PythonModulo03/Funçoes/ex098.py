84d81bbc8df4e983e785f760767b595868fe5e56
Francisco Goya <franciscogoya.amc@gmail.com>
2024-09-12 18:26:36 -0300

    Update ex098.py

--- CursoEmVideo/PythonModulo03/Funçoes/ex098.py
+++ CursoEmVideo/PythonModulo03/Funçoes/ex098.py
@@ -1,3 +1,9 @@
+"""
+Faça um programa que tenha uma função chamada contador(), que receba três parâmetros: início, fim e passo. Seu programa tem que realizar três contagens através da função criada:
+a) de 1 até 10, de 1 em 1
+b) de 10 até 0, de 2 em 2
+c) uma contagem personalizada
+"""
 from time import sleep
 
 def escreva(txt):
@@ -6,22 +12,31 @@ def escreva(txt):
     print('-'*len(txt))
 
 def contador(inicio, fim, passo):
-    escreva(f'Contando de {inicio} até {fim} andando de {abs(passo)} em {abs(passo)}')
+    if passo < 0:
+        passo = abs(passo)
+    if passo == 0:
+        passo = 1
+    escreva(f'Contando de {inicio} até {fim} andando de {passo} em {passo}')
     if inicio < fim:
-        for i in range(inicio, fim+1, passo):
-            print(f'{i}', end=' ')
+        c = inicio
+        while c <= fim:
+            print(f'{c}', end=' ', flush=True)
+            c += passo
+            sleep(0.5)
     else:
-        if passo < 0:
-            for i in range(inicio, fim, passo):
-                print(f'{i}', end=' ')
-        for i in range(inicio, fim, - passo):
-            print(f'{i}', end=' ')
+        c = inicio
+        while c >= fim:
+            print(f'{c}', end=' ', flush=True)
+            c -= passo
+            sleep(0.5)
     print('Fim')
 
-contador(1,10,1)
-contador(20,0,8)
 
-inicio = int(input('A partir de que número devo iniciar a contagem? '))
+contador(1,10,1)
+contador(10,0,2)
+escreva('Agora é a sua vez!')
+
+inicio = int(input('A partir de que número devo contar? '))
 fim = int(input('Até que número devo contar? '))
 passo = int(input('De quantos em quantos números devo contar? '))
 
